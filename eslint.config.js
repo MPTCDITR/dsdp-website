e2e62b33ec7a743d8f8506ed210a746718092050
avearistov <chantepkik@gmail.com>
2025-07-07 22:39:46 +0700

    batman

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
+import astroPlugin from "eslint-plugin-astro";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,astro}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+    plugins: {
+      astro: astroPlugin,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/no-unknown-property": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "prettier/prettier": "off",
+      "react/jsx-no-undef": "off",
+      "astro/no-conflict-set-directives": "error",
+      "astro/no-unused-define-vars-in-style": "error",
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", ".astro/", "public/"],
+  },
+];
---
+++ prettier.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  arrowParens: "always",
+  trailingComma: "all",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "",
+    "^@/app/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "",
+    "^[./]",
+    "<THIRD_PARTY_MODULES>",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: ["prettier-plugin-astro", "@ianvs/prettier-plugin-sort-imports"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
