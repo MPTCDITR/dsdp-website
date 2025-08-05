bd6d688c604eb9d00b1ccdf19fd55b518d573971
Lyching Kong <lychingkong@gmail.com>
2025-08-05 11:23:35 +0700

    fix image not loading when run preview

--- src/data/HowToApplyData.ts
+++ src/data/HowToApplyData.ts
@@ -1,6 +1,9 @@
 import React from "react";
 
 import { Content1, Content2, Content3 } from "./HowToApplyContent";
+import image1 from "/src/assets/images/image1.webp";
+import image2 from "/src/assets/images/image2.webp";
+import image3 from "/src/assets/images/image3.webp";
 
 export const HOWTOAPPLY_TABS = {
   STUDENT: "studentS",
@@ -20,21 +23,21 @@ export const HowToApplyData: Array<HowToApplyDataProps> = [
   // Student Criteria
   {
     header_number: "01",
-    image: "/src/assets/images/image1.webp",
+    image: image1.src,
     title: "លក្ខខណ្ឌទី១៖ លក្ខខណ្ឌតម្រូវមូលដ្ឋាន",
     content: Content1,
     type_content: HOWTOAPPLY_TABS.STUDENT,
   },
   {
     header_number: "02",
-    image: "/src/assets/images/image2.webp",
+    image: image2.src,
     title: "លក្ខខណ្ឌទី២៖ សំណុំលិខិតភ្ជាប់នៅពេលបំពេញពាក្យចុះឈ្មោះស្នើសុំដំបូង",
     content: Content2,
     type_content: HOWTOAPPLY_TABS.STUDENT,
   },
   {
     header_number: "03",
-    image: "/src/assets/images/image3.webp",
+    image: image3.src,
     title: "លក្ខខណ្ឌទី៣៖ សំណុំលិខិតភ្ជាប់នៅក្រោយពេលជាប់ជាស្ថាពរ",
     content: Content3,
     type_content: HOWTOAPPLY_TABS.STUDENT,
@@ -43,21 +46,21 @@ export const HowToApplyData: Array<HowToApplyDataProps> = [
   // Officer Criteria
   {
     header_number: "01",
-    image: "/src/assets/images/image1.webp",
+    image: image1.src,
     title: "លក្ខខណ្ឌទី១៖ លក្ខខណ្ឌតម្រូវមូលដ្ឋាន",
     content: Content1,
     type_content: HOWTOAPPLY_TABS.OFFICER,
   },
   {
     header_number: "02",
-    image: "/src/assets/images/image2.webp",
+    image: image2.src,
     title: "លក្ខខណ្ឌទី២៖ សំណុំលិខិតភ្ជាប់នៅពេលបំពេញពាក្យចុះឈ្មោះស្នើសុំដំបូង",
     content: Content2,
     type_content: HOWTOAPPLY_TABS.OFFICER,
   },
   {
     header_number: "03",
-    image: "/src/assets/images/image3.webp",
+    image: image3.src,
     title: "លក្ខខណ្ឌទី៣៖ សំណុំលិខិតភ្ជាប់នៅក្រោយពេលជាប់ជាស្ថាពរ",
     content: Content3,
     type_content: HOWTOAPPLY_TABS.OFFICER,
