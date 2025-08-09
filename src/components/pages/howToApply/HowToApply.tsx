fc2208dc7f7df1724a001c05afa6437dcd88ceaa
Seng SeangLeng <seangleng507@gmail.com>
2025-08-09 12:21:01 +0700

    Enhance how-to-apply functionality by implementing tab navigation and updating footer links. Adjusted tab values for consistency and improved URL handling for dynamic tab selection.

--- src/components/pages/howToApply/HowToApply.tsx
+++ src/components/pages/howToApply/HowToApply.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import HowApplyStepByTabs from "./HowApplyStepByTabs";
-import { HOWTOAPPLY_TABS } from "@/data/HowToApplyData";
+import {
+  HOWTOAPPLY_TABS,
+  type HowToApply as HowToApplyType,
+} from "@/data/HowToApplyData";
 
 export function HowToApply() {
+  const [currentTab, setCurrentTab] = useState<HowToApplyType>(
+    HOWTOAPPLY_TABS.STUDENT,
+  );
+
+  useEffect(() => {
+    const tab = new URLSearchParams(window.location.search).get("tab");
+    if (tab) {
+      setCurrentTab(tab as HowToApplyType);
+    }
+  }, []);
+
   return (
     <div className="container mx-auto my-4 px-4 bg-muted rounded-xl h-auto flex flex-col p-6 gap-7 mb-16">
-      {/* <!-- Tabs --> */}
-      <Tabs defaultValue={HOWTOAPPLY_TABS.STUDENT}>
+      <Tabs
+        defaultValue={HOWTOAPPLY_TABS.STUDENT}
+        value={currentTab}
+        onValueChange={setCurrentTab}
+      >
         <TabsList>
           <TabsTrigger value={HOWTOAPPLY_TABS.STUDENT}>
             សម្រាប់សិស្ស-និស្សិត
