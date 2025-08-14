c125dad20bb31c6e575c9adde249183a239939d7
dana <lydana32@gmail.com>
2025-08-14 15:01:04 +0700

    fix: fix issue 54

--- src/components/pages/about/about-team-content.tsx
+++ src/components/pages/about/about-team-content.tsx
@@ -1,6 +1,6 @@
 import AboutTeamCard from "./components/AboutTeamCard";
 import { team_data } from "./data/aboutTeamData";
-import StructureImg from "@/assets/about/about-team/structure-image.png";
+import StructureImg from "@/assets/about/about-team/structure-img.svg";
 import type { Language } from "@/i18n/ui";
 import { useTranslations } from "@/i18n/utils";
 
@@ -8,31 +8,23 @@ export function AboutTeam({ lang }: { lang: Language }) {
   const t = useTranslations(lang);
 
   return (
-    <div>
-      {/* Top section */}
-      <section className="text-center mt-12 mb-8">
-        <h1 className="text-gradient-r text-4xl font-bold leading-snug mb-4">
-          {t("about.team.title1")}
-        </h1>
-        <img
-          src={StructureImg.src}
-          alt="Structure"
-          className="w-full"
-          loading="lazy"
-        />
-      </section>
-
-      <section className="bg-gray-50 py-8">
-        <div className="container relative mx-auto">
-          <header className="mx-auto mb-4 max-w-5xl">
-            <h2 className="text-center text-4xl font-bold text-primary mb-4">
+    <div className="container mx-auto">
+      <img
+        src={StructureImg.src}
+        alt="Structure"
+        className="w-full my-16"
+        loading="lazy"
+      />
+      <section className="relative left-1/2 right-1/2 -mx-[50vw] w-screen bg-gray-50 py-8">
+        <div className="container mx-auto my-8">
+          <header className="mx-auto max-w-5xl text-center">
+            <h2 className="text-4xl font-bold text-primary mb-6">
               {t("about.team.title2")}
             </h2>
-            <p className="text-center text-base text-muted-foreground">
+            <p className="text-base text-muted-foreground mb-6">
               {t("about.team.title2.description")}
             </p>
           </header>
-
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {team_data.map(({ id, name, position, img, contacts }) => (
               <AboutTeamCard
